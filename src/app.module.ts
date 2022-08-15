717fb1d6b892c62125367f39c77701d9ef3ef483
StayAndGo <wthuck@163.com>
2022-08-15 23:48:17 +0800

    fix: update db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'mima999.',
+      password: 'database999',
       database: 'test',
       entities: [],
       synchronize: true,
